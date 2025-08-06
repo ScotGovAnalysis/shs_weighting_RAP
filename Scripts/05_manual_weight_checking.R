671200bfb7863956b301613b4aa34b7ee213091b
ClodaghS <clodagh.smith@gov.scot>
2025-08-06 10:11:33 +0100

    Added more automation to the weight checking

--- Scripts/05_manual_weight_checking.R
+++ Scripts/05_manual_weight_checking.R
@@ -45,6 +45,16 @@ ind_large_wts <- read_csv(here::here("Outputs", "large_randad_wts.csv"))
 
 kid_large_wts <- read_csv(here::here("Outputs", "large_randsc_wts.csv"))
 
+hh_est <- read_csv(config$hhpoptotals.path)
+
+ind_est <- read_csv(config$adultpoptotals.path)
+
+kid_est <- read_csv(config$kidpoptotals.path)
+
+prev_year <- read_csv(paste0(config$prevyear.path, "shs_weights_", config$prevdate, ".csv"))
+
+prev_trav <- read_csv(paste0(config$prevyear.path, "shs_travweight_", config$prevdate, ".csv"))
+
 wb <- loadWorkbook(here::here("Outputs", "SHS final weight checking.xlsx"))
 
 
@@ -58,41 +68,79 @@ writeData(wb, sheet = "hhwts", x = hhwts_tab, startRow = 2,
           startCol = 2, colNames = FALSE, rowNames = FALSE)
 
 
-### 3 - hh_calib tab ----
+### 3 - dweight tab ----
+dweight_tab <- hh_est %>%
+  select(2)
+
+writeData(wb, sheet = "dweight", x = dweight_tab, startRow = 2, 
+          startCol = 2, colNames = FALSE, rowNames = FALSE)
+
+### 4 - hh_calib tab ----
 hh_calib <- hhwts[, c(1, 3, 8:11)]
 
 writeData(wb, sheet = "hh_calib", x = hh_calib, startRow = 2, 
           startCol = 2, colNames = FALSE, rowNames = FALSE)
 
 
-### 4 - shsind tab ----
+### 5 - shsind tab ----
 writeData(wb, sheet = "shsind", x = adwts, startRow = 2, 
           startCol = 2, colNames = FALSE, rowNames = FALSE)
 
 
-### 5 - IndLargeVals tab ----
+### 6 - ind_wt tab ----
+ind_wt_tab <- ind_est %>% 
+  select(2) %>%
+  slice(c(1:12, 14:20, 13, 21:n()))
+
+writeData(wb, sheet = "ind_wt", x = ind_wt_tab, startRow = 2, 
+          startCol = 2, colNames = FALSE, rowNames = FALSE)
+
+### 7 - IndLargeVals tab ----
 writeData(wb, sheet = "IndLargeVals", x = ind_large_wts, startRow = 2, 
           startCol = 1, colNames = FALSE, rowNames = FALSE)
 
 
-### 6 - shskid tab ----
+### 8 - shskid tab ----
 writeData(wb, sheet = "SHSkid", x = kidwts, startRow = 2, 
           startCol = 2, colNames = FALSE, rowNames = FALSE)
 
 
-### 7 - KidLargeValues tab ----
+### 9 - kid_wt tab ----
+kid_wt_tab <- kid_est %>% 
+  select(2) %>%
+  slice(c(1:12, 14:20, 13, 21:n()))
+
+writeData(wb, sheet = "kid_wt", x = kid_wt_tab, startRow = 2, 
+          startCol = 3, colNames = FALSE, rowNames = FALSE)
+
+
+### 10 - KidLargeValues tab ----
 writeData(wb, sheet = "KidLargeValues", x = kid_large_wts, startRow = 2, 
           startCol = 1, colNames = FALSE, rowNames = FALSE)
 
 
-### 8 - Trav weights tab ----
+### 11 - prev_year tab ----
+prev_year_wts <- prev_year %>% 
+  select(1,2,4,6)
+
+writeData(wb, sheet = "prev_year", x = prev_year_wts, startRow = 2, 
+          startCol = 1, colNames = FALSE, rowNames = FALSE)
+
+
+### 12 - Trav weights tab ----
 trav_check <- travwts[, 2, drop = FALSE]
 
 writeData(wb, sheet = "Trav weights", x = trav_check, startRow = 2, 
           startCol = 2, colNames = FALSE, rowNames = FALSE)
 
 
-### 9 - Save workbook ----
+prev_trav_wts <- prev_trav [, 2, drop = FALSE]
+
+writeData(wb, sheet = "Trav weights", x = prev_trav_wts, startRow = 2, 
+          startCol = 1, colNames = FALSE, rowNames = FALSE)
+
+
+### 13 - Save workbook ----
 
 message("Exporting workbook")
 
